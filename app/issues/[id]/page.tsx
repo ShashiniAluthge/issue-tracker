e3bfacd11804d19b47502a60cbd3c12a9be7f6ee
Shashini Aluthge <ssshashini21@gmail.com>
2025-12-15 15:49:32 +0530

    Add additional loading skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import prisma from '@/prisma/client';
 import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
+import delay from 'delay';
 
 interface Props {
     params: Promise<{ id: string }>;
@@ -18,6 +19,8 @@ const IssueDetailPage = async ({ params }: Props) => {
 
     if (!issue)
         notFound();
+
+    await delay(2000);
 
     return (
         <div>

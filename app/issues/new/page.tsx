0852abd34f8de5f712fa40e71b181da32f6cf18d
Shashini Aluthge <ssshashini21@gmail.com>
2025-12-15 19:47:54 +0530

    Build the edit issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,88 +1,9 @@
-'use client'
-import ErrorMessage from '@/app/components/ErrorMessage'
-import Spinner from '@/app/components/Spinner'
-import { createIssueSchema } from '@/app/validationSchemas'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Button, Callout, TextField } from '@radix-ui/themes'
-import axios from 'axios'
-import 'easymde/dist/easymde.min.css'
-import dynamic from 'next/dynamic'
-import { useRouter } from 'next/navigation'
-import { useMemo, useState } from 'react'
-import { Controller, useForm } from 'react-hook-form'
-import { z } from "zod"
+import IssueForm from "../components/IssueForm"
 
-// Dynamically import SimpleMDE with SSR disabled
-const SimpleMdeReact = dynamic(() => import('react-simplemde-editor'), {
-    ssr: false,
-})
-
-
-type IssueForm = z.infer<typeof createIssueSchema>;
-
-// interface IssueForm {               //instead of this interface we use this infer so we no need to update multiple places in future
-//     title: string;
-//     description: string;
-// }
 
 const NewIssuePage = () => {
-    const router = useRouter();
-    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({ resolver: zodResolver(createIssueSchema) });
-
-    const [error, setError] = useState('');
-    const [isSubmitting, setSubmitting] = useState(false);
-
-    // Memoize the options to prevent unnecessary re-renders
-    const simpleMdeOptions = useMemo(() => ({
-        placeholder: "Description"
-    }), []);
-
-    const onSubmit = handleSubmit(async (data) => {
-        setSubmitting(true)
-        try {
-            await axios.post('/api/issues', data);
-            router.push('/issues');
-        } catch (error) {
-            setSubmitting(false)
-            setError('An Unexpected Error Occur!')
-        }
-
-    })
-
-
     return (
-        <div className='max-w-xl '>
-            {error && <Callout.Root color='red' className='mb-5'>
-
-                <Callout.Text >
-                    {error}
-                </Callout.Text>
-            </Callout.Root>
-            }
-            <form className='space-y-3' onSubmit={onSubmit} suppressHydrationWarning>
-                <TextField.Root placeholder="Title" {...register('title')}>
-                </TextField.Root>
-                <ErrorMessage>
-                    {errors.title?.message}
-                </ErrorMessage>
-                <Controller
-                    name='description'
-                    control={control}
-                    defaultValue=''
-                    render={({ field }) => (
-                        <SimpleMdeReact
-                            options={simpleMdeOptions}
-                            value={field.value}
-                            onChange={field.onChange}
-                        />
-                    )}
-                />
-                <ErrorMessage>
-                    {errors.description?.message}
-                </ErrorMessage>
-                <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
-            </form>
-        </div>
+        <IssueForm />
     )
 }
 
